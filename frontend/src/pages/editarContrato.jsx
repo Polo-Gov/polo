a9af1ef9418eed759a7b32e5782c0595e0e2ef53
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2022-12-11 20:05:42 -0300

    Update editarContrato.jsx

--- frontend/src/pages/editarContrato.jsx
+++ frontend/src/pages/editarContrato.jsx
@@ -2,33 +2,54 @@ import { useParams } from "react-router";
 import { useNavigate } from "react-router";
 import NavBar from "../components/navBar";
 import arrowLeft from "../assets/arrow-left.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const EditarContrato = () => {
   const param = useParams();
   const endereço = param.id;
-  let oi = false
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  let infosContrato = {
-    duracao: 12,
-    tipo: "aluguel",
-    ultimaFisca: 12 / 12 / 2021,
-    proximaFisca: 12 / 12 / 2021,
-    valor: 1000,
-    ultimaCobra: 12 / 12 / 2021,
-    status: "ativo",
-  };
+  const [duracao, setDuracao] = useState(12);
+  const [tipo, setTipo] = useState("aluguel");
+  const [ultimaFisca, setUltimaFisca] = useState("2012-04-13");
+  const [proximaFisca, setProximaFisca] = useState("2024-11-12");
+  const [valor, setValor] = useState(1000);
+  const [ultimaCobra, setUltimaCobra] = useState("2022-11-27");
+  const [status, setStatus] = useState("ativo");
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    
+    // COLOCAR AS REQUESTS AQUI
+    // O ENDEREÇO DO CONTRATO EM QUESTÃO ESTÁ NA VARIAVEL "endereço"
+    
+    // AS INFORMAÇÕES DO CONTRATO DEVEM ENTRAR NOS useState DA LINHA 12 a 18
+    
+    // useState que tira da tela o "Carregando..." e mostra o conteúdo da página
+    setIsLoading(false)
+  },[])
 
+  const sendEditedContract = () => {
+    // COLOCAR O POST DO ENVIO DA EDIÇÃO DO CONTRATO AQUI
 
+    // volta para a página de imóveis
+    navigate("/imóveis")
+  }
+
+    if (isLoading) {
+      return <div className="text-center mt-16 text-blueGov font-bold text-3xl">Carregando...</div>;
+    }
   return (
+  
     <div className="text-center content-center">
       <NavBar></NavBar>
       <div className="p-5 absolute">
         <img
           src={arrowLeft}
           alt=""
-          onClick={()=>{navigate("/imóveis")}}
+          onClick={() => {
+            navigate("/imóveis");
+          }}
           className="cursor-pointer"
         />
       </div>
@@ -43,40 +64,73 @@ const EditarContrato = () => {
       <div className="w-3/5 m-auto">
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Duração</p>
-          <h1 className="font-sans font-semibold ">{infosContrato.duracao}</h1>
+          <input
+            type={"number"}
+            value={duracao}
+            onChange={(e) => setDuracao(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Tipo</p>
-          <h1 className="font-sans font-semibold ">{infosContrato.tipo}</h1>
-        </div>
+          <input
+            type={"text"}
+            value={tipo}
+            onChange={(e) => setTipo(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
+        </div>{" "}
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Última fiscalização</p>
-          <h1 className="font-sans font-semibold ">
-            {infosContrato.ultimaFisca}
-          </h1>
+          <input
+            type={"date"}
+            value={ultimaFisca}
+            onChange={(e) => setUltimaFisca(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Próxima fiscalização</p>
-          <h1 className="font-sans font-semibold ">
-            {infosContrato.proximaFisca}
-          </h1>
+          <input
+            type={"date"}
+            value={proximaFisca}
+            onChange={(e) => setProximaFisca(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
-          <p className="font-sans font-light text-xl">Valor</p>
-          <h1 className="font-sans font-semibold ">{infosContrato.valor}</h1>
+          <p className="font-sans font-light text-xl">Valor (R$)</p>
+          <input
+            type={"number"}
+            value={valor}
+            onChange={(e) => setValor(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Última cobrança</p>
-          <h1 className="font-sans font-semibold ">
-            {infosContrato.ultimaCobra}
-          </h1>
+          <input
+            type={"data"}
+            value={ultimaCobra}
+            onChange={(e) => setUltimaCobra(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
         <div className="flex justify-between border-b-2 border-black items-center mb-3">
           <p className="font-sans font-light text-xl">Status</p>
-          <h1 className="font-sans font-semibold ">{infosContrato.status}</h1>
+          <input
+            type={"text"}
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+            className=" rounded-lg border-slate-500 pl-2 pr-2 border-2"
+          ></input>
         </div>
       </div>
+      <button onClick={sendEditedContract} className="p-3 bg-blueGov text-white rounded-lg mb-10 font-bold">
+        Salvar alterações
+      </button>
     </div>
+  
   );
 };
 export default EditarContrato;

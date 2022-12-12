e676ad12d00d54b9462326441f3b6a209f0a852a
Lemos1347 <99190347+Lemos1347@users.noreply.github.com>
2022-12-11 22:30:57 -0300

    Update interactions.js

--- frontend/src/ethereum/interactions.js
+++ frontend/src/ethereum/interactions.js
@@ -6,15 +6,15 @@ const getProvider = async () => {
   return provider;
 };
 
-export const getAllDonos = async () => {
-  const imovelContract = await imovelInstance();
+export const getAllDonos = async (idImovel) => {
+  const imovelContract = await imovelInstance(idImovel);
   const allDonos = await imovelContract.getAllDonos();
   return allDonos;
 };
 
-export const getDonoId = async (address) => {
+export const getDonoId = async (idImovel, address) => {
   let donoId;
-  const allDonos = await getAllDonos();
+  const allDonos = await getAllDonos(idImovel);
   for (let i = 0; i < allDonos.length; i++) {
     if (allDonos[i] === address) {
       donoId = i;
@@ -23,56 +23,56 @@ export const getDonoId = async (address) => {
   return donoId;
 };
 
-export const getAcoesCount = async (address) => {
-  const imovelContract = await imovelInstance();
+export const getAcoesCount = async (idImovel, address) => {
+  const imovelContract = await imovelInstance(idImovel);
   const donoId = await getDonoId(address);
   const count = await imovelContract.acoesCount(donoId);
   return count;
 };
 
-export const getCondicoes = async (address, number) => {
-  const imovelContract = await imovelInstance();
+export const getCondicoes = async (idImovel, address, number) => {
+  const imovelContract = await imovelInstance(idImovel);
   const donoId = await getDonoId(address);
   const condicoes = await imovelContract.condicoes(donoId, number);
   return condicoes;
 };
 
-export const getDataProxCobranca = async (address) => {
-  const imovelContract = await imovelInstance();
+export const getDataProxCobranca = async (idImovel, address) => {
+  const imovelContract = await imovelInstance(idImovel);
   const donoId = await getDonoId(address);
   const data = await imovelContract.getDataProxCobranca(donoId);
   return data;
 };
 
-export const getOneDono = async (id) => {
-  const imovelContract = await imovelInstance();
+export const getOneDono = async (idImovel, id) => {
+  const imovelContract = await imovelInstance(idImovel);
   const address = await imovelContract.donos(id);
   return address;
 };
 
-export const getValorCobranca = async (address) => {
-  const imovelContract = await imovelContract();
+export const getValorCobranca = async (idImovel, address) => {
+  const imovelContract = await imovelContract(idImovel);
   const donoId = await getDonoId(address);
   const valor = await imovelContract.valorCobranca(donoId);
   return valor;
 };
 
-export const getStatus = async (address) => {
-  const imovelContract = await imovelContract();
+export const getStatus = async (idImovel, address) => {
+  const imovelContract = await imovelContract(idImovel);
   const donoId = await getDonoId(address);
   const status = await imovelContract.status(donoId);
   return status;
 };
 
-export const getPrazo = async (address) => {
-  const imovelContract = await imovelContract();
+export const getPrazo = async (idImovel, address) => {
+  const imovelContract = await imovelContract(idImovel);
   const donoId = await getDonoId(address);
   const prazo = await imovelContract.prazo(donoId);
   return prazo;
 };
 
-export const getHistoricoRecebimento = async (address, idHist) => {
-  const imovelContract = await imovelContract();
+export const getHistoricoRecebimento = async (idImovel, address, idHist) => {
+  const imovelContract = await imovelContract(idImovel);
   const donoId = await getDonoId(address);
   const hist = await getHistoricoRecebimento(address, idHist);
   // hist = {data: unixtime, valor: unixtime}
@@ -81,14 +81,14 @@ export const getHistoricoRecebimento = async (address, idHist) => {
   return hist;
 };
 
-export const getIdPredio = async () => {
-  const imovelContract = await imovelContract();
+export const getIdPredio = async (idImovel) => {
+  const imovelContract = await imovelContract(idImovel);
   const idPredio = await imovelContract.idPredio();
   return idPredio;
 };
 
-export const getIdAcoes = async (address, number) => {
-  const imovelContract = await imovelContract();
+export const getIdAcoes = async (idImovel, address, number) => {
+  const imovelContract = await imovelContract(idImovel);
   const donoId = await getDonoId(address);
   const acao = await imovelContract.getIdAcoes(donoId, number);
   const response = await fetch(acao).then((res) => {
